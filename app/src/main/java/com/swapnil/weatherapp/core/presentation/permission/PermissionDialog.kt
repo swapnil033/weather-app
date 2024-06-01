a6f74f0d39be46e9453fae2918b98678fab5b55f
swapnil033 <swapnildev33@gmail.com>
2024-06-01 12:06:16 +0530

    added user permission code

---
+++ app/src/main/java/com/swapnil/weatherapp/core/presentation/permission/PermissionDialog.kt
@@ -0,0 +1,73 @@
+package com.swapnil.weatherapp.core.presentation.permission
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.swapnil.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PermissionDialog(
+    permissionTextProvider: PermissionTextProvider,
+    isPermanentlyDeclined: Boolean,
+    onDismiss: () -> Unit,
+    onOkClick: () -> Unit,
+    onGoToAppSettingsClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    BasicAlertDialog(
+        onDismissRequest = onDismiss,
+        modifier = modifier
+    ) {
+        Column {
+            Text(text = "Permission required")
+            Text(
+                text = permissionTextProvider.getDescription(
+                    isPermanentlyDeclined = isPermanentlyDeclined
+                )
+            )
+            Divider()
+            Text(
+                text = if(isPermanentlyDeclined) {
+                    "Grant permission"
+                } else {
+                    "OK"
+                },
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        if (isPermanentlyDeclined) {
+                            onGoToAppSettingsClick()
+                        } else {
+                            onOkClick()
+                        }
+                    }
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PermissionDialogPreview(){
+    WeatherAppTheme{
+        //PermissionDialog()
+    }
+}
+interface PermissionTextProvider {
+    fun getDescription(isPermanentlyDeclined: Boolean): String
+}
