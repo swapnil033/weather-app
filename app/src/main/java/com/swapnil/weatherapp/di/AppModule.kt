b34a2043e0f3df4eb09ce248683732f76d846ea2
swapnil033 <swapnildev33@gmail.com>
2024-06-18 11:08:45 +0530

    added http interceptor

--- app/src/main/java/com/swapnil/weatherapp/di/AppModule.kt
+++ app/src/main/java/com/swapnil/weatherapp/di/AppModule.kt
@@ -12,6 +12,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -22,9 +25,23 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): WeatherApi {
+    fun provideClient() : OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): WeatherApi {
         return Retrofit.Builder().baseUrl("https://api.open-meteo.com/")
-            .addConverterFactory(MoshiConverterFactory.create()).build()
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
             .create(WeatherApi::class.java)
     }
 
